2c56981d710316c8e0af910d971adc9a452234bd
Zulsar Batmunkh <zulsarbatmunkh@Zulsars-MacBook-Air.local>
2021-12-21 14:40:50 +0800

    All

--- server.js
+++ server.js
@@ -175,10 +175,9 @@ app.use("/api/restaurants", restaurantsRouter);
 app.use("/api/users", usersRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
