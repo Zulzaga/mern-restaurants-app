ad6106057c61461d4f302211ae365d42b7df2b1f
Zulsar Batmunkh <zulsarbatmunkh@Zulsars-MacBook-Air.local>
2021-12-21 14:45:17 +0800

    All

--- server.js
+++ server.js
@@ -174,12 +174,10 @@ const usersRouter = require("./routes/users");
 app.use("/api/restaurants", restaurantsRouter);
 app.use("/api/users", usersRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
