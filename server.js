23c963c6c81fb512e45e5ac16f6ccfdba667ed07
Zulsar Batmunkh <zulsarbatmunkh@Zulsars-MacBook-Air.local>
2021-12-21 14:37:04 +0800

    All

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static("client/build"));
-
 app.use(cors());
 app.use(express.json());
 
@@ -176,9 +174,13 @@ const usersRouter = require("./routes/users");
 app.use("/api/restaurants", restaurantsRouter);
 app.use("/api/users", usersRouter);
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client/build/index.html"));
-// });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
